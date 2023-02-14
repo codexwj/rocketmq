7bb124c724db604188e1a3fe9afb2daddcb396be
xiewj25 <xiewj25@midea.com>
2023-02-14 10:07:20 +0800

    update

--- src/main/java/com/codejames/rocketmq/controller/ProducerController.java
+++ src/main/java/com/codejames/rocketmq/controller/ProducerController.java
@@ -9,6 +9,7 @@ import static org.springframework.messaging.support.MessageBuilder.withPayload;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.messaging.Message;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -19,7 +20,8 @@ import java.util.UUID;
  * @author xiewj25
  * @date 2023/2/13 17:15
  */
-@RestController("/producer")
+@RestController()
+@RequestMapping("/producer")
 public class ProducerController {
 
     @Resource
@@ -34,6 +36,7 @@ public class ProducerController {
         producerDto.setReqId(UUID.randomUUID().toString());
 
         Message<ProducerDto> message = withPayload(producerDto).setHeader(PROPERTY_KEYS, producerDto.getReqId()).build();
+        System.out.println("生产的消息为：" + message);
         rocketMQTemplate.syncSend(TOPIC_DEMO, message);
     }
 }
--- src/main/java/com/codejames/rocketmq/controller/mq/Consumer.java
+++ src/main/java/com/codejames/rocketmq/controller/mq/Consumer.java
@@ -3,6 +3,9 @@ package com.codejames.rocketmq.controller.mq;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.Charset;
 
 import static com.codejames.rocketmq.config.Constants.TOPIC_DEMO;
 
@@ -11,13 +14,20 @@ import static com.codejames.rocketmq.config.Constants.TOPIC_DEMO;
  * @date 2023/2/13 17:55
  */
 @RocketMQMessageListener(
+        consumeThreadNumber = 29,
         topic = TOPIC_DEMO,
         consumerGroup = "consumer_demo_group"
 
 )
+@Component
 public class Consumer implements RocketMQListener<MessageExt> {
+
+    private final String CHARSET = Charset.defaultCharset().name();
+
     @Override
     public void onMessage(MessageExt message) {
-
+        byte[] body = message.getBody();
+        String str = new String(body, Charset.forName(this.CHARSET));
+        System.out.println("消费者消费的消息为： " + str);
     }
 }
