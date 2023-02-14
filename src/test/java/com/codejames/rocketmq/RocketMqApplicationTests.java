44bc31344f632360ef4411a5d1807d42d5341c0f
xiewj25 <xiewj25@midea.com>
2023-02-14 13:53:36 +0800

    update

---
+++ src/test/java/com/codejames/rocketmq/RocketMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codejames.rocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
